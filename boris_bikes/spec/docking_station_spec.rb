c40cd14f4c8f14c092d47d20c3b910db54ff396a
Shoniwa Marovatsanga <stmarovatsanga@gmail.com>
2017-01-05 16:32:39 +0000

    exercise 11

--- boris_bikes/spec/docking_station_spec.rb
+++ boris_bikes/spec/docking_station_spec.rb
@@ -2,9 +2,16 @@ require 'docking_station'
 
 describe DockingStation do
 
-bike = Bike.new 
+bike = Bike.new
 
   it { is_expected.to respond_to :release_bike }
-  it { allow(subject).to receive(:release_bike).and_return(Bike.new)}
+  it { allow(subject).to receive(:release_bike).and_return(bike)}
   it { expect(bike.working?).to eq true }
+  it { is_expected.to respond_to :dock_bike }
+  it { is_expected.to respond_to(:dock_bike).with(1).argument }
+  it 'returns a docked bike' do
+#    bike = Bike.new
+   subject.dock_bike(bike)
+   expect(subject.bike).to eq bike
+ end
 end
