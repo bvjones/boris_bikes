1e5ac990e157d7a812ea2264e341cfc5225484b0
bvjones <benvaughanjones@gmail.com>
2017-01-05 12:33:39 +0000

    ex 10

--- boris_bikes/spec/docking_station_spec.rb
+++ boris_bikes/spec/docking_station_spec.rb
@@ -1,5 +1,10 @@
 require 'docking_station'
 
 describe DockingStation do
+
+bike = Bike.new 
+
   it { is_expected.to respond_to :release_bike }
+  it { allow(subject).to receive(:release_bike).and_return(Bike.new)}
+  it { expect(bike.working?).to eq true }
 end
